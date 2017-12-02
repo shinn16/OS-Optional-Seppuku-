2176be7fc145bd782506e96e2420b8cb35bab033
Patrick Shinn <shinn5112@gmail.com>
2017-12-02 13:06:19 -0500

    DMA methods and class implemented

--- OS-Optional-Seppuku/src/Memory.java
+++ OS-Optional-Seppuku/src/Memory.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Memory {
 
     private int PAGE_SIZE;
     private int pageNumber = 0;
-
+    private DMA dma;
     private int[] memory1 = new int[128]; //if each page holds 4 items then one pages corresponds to 4 places in memory
     public Memory(int PAGE_SIZE){
         this.PAGE_SIZE = PAGE_SIZE;
@@ -27,5 +28,51 @@ public class Memory {
         int offset = addressVector.getValue();
         int physicalAddress = pageNumber * PAGE_SIZE + offset;
         return memory1[physicalAddress];
+    }
+
+    public void removeDMA(){
+        dma.removeDMA();
+    }
+
+    public int accessDMA(){
+       return dma.accessDMA();
+    }
+
+    public void writeDMA(int data){
+        dma.writeDMA(data);
+    }
+}
+
+class DMA implements Runnable{
+    private boolean dmaFlag = true, IOFlag = true;
+    private ArrayList<Integer> memory2 = new ArrayList<>();
+
+    @Override
+    public void run() {
+        if (dmaFlag && IOFlag){
+            int y = accessDMA();
+            System.out.println(y);
+            setDmaFlag(false);
+        }
+    }
+
+    public void setDmaFlag(boolean dmaFlag) {
+        this.dmaFlag = dmaFlag;
+    }
+
+    public void setIOFlag(boolean IOFlag) {
+        this.IOFlag = IOFlag;
+    }
+
+    public int accessDMA(){
+        return memory2.get(0);
+    }
+
+    public void removeDMA(){
+        memory2.remove(0);
+    }
+
+    public void writeDMA(int data){
+        memory2.add(data);
     }
 }
